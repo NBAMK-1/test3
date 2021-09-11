d5295aead96e69479c2311c35f35a2892185212e
Nishant Bainsla <65064852+NBAMK-1@users.noreply.github.com>
2021-09-11 08:39:07 +0530

    Add files via upload
    it have pagination working but with no data

---
+++ test 3/src/main/java/com/example/test3/BookUtils.java
@@ -0,0 +1,25 @@
+package com.example.test3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class BookUtils {
+    private static List<Book> books = new ArrayList<Book>();
+
+    private static final int NUM_BOOKS = 30;
+
+    private static final int MIN_BOOK_NUM = 1000;
+
+    public static List<Book> buildBooks() {
+        if (books.isEmpty()) {
+            IntStream.range(0, NUM_BOOKS).forEach(n -> {
+                books.add(new Book(MIN_BOOK_NUM + n + 1, "Spring in Action"));
+            });
+
+        }
+
+        return books;
+    }
+
+}
